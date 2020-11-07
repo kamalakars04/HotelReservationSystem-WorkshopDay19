25ff79dd7c5ea227629dc6892605da3b02339521
kamalakars04 <kamalakars04@gmail.com>
2020-11-07 12:18:27 +0530

    [Kamalakar].[Refactor].Added UC2_GetCheapestHotel and TestCase

--- HotelDetails.cs
+++ HotelDetails.cs
@@ -6,6 +6,7 @@ namespace HotelReservationSystemWorkshop
 {
     public class HotelDetails
     {
+
         // Variables
         public string hotelName;
         public double totalFare;
@@ -20,6 +21,47 @@ namespace HotelReservationSystemWorkshop
         {
             this.hotelName = hotelName;
             this.costPerDay = costPerDay;
+        }
+
+        /// <summary>
+        /// UC 2 Gets the total fare.
+        /// </summary>
+        /// <param name="startDate">The start date.</param>
+        /// <param name="endDate">The end date.</param>
+        /// <returns></returns>
+        public double GetTotalFare(DateTime startDate, DateTime endDate)
+        {
+            // Continue loop till all the dates are covered
+            while (startDate != endDate.AddDays(1))
+            {
+                totalFare += costPerDay;
+                startDate = startDate.AddDays(1);
+            }
+            return totalFare;
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            // If obj passed is null
+            if (obj == null)
+                return false;
+
+            // If object passed is of other datatype
+            if (!(obj is HotelDetails))
+                return false;
+
+            // Convert the obj into Hotel Details object
+            HotelDetails hotelOutput = ((HotelDetails)obj);
+
+            // return true if hotel name are same else false
+            return this.hotelName == hotelOutput.hotelName;
         }
     }
 }
--- HotelReservationSystem.cs
+++ HotelReservationSystem.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace HotelReservationSystemWorkshop
 {
-    class HotelReservationSystem : IAdmin
+    public class HotelReservationSystem : IAdmin,Icustomer
     {
         static Dictionary<string, HotelDetails> regularHotelMap = new Dictionary<string, HotelDetails>();
 
@@ -31,6 +31,48 @@ namespace HotelReservationSystemWorkshop
             {
                 regularHotelMap.Add(hotel.hotelName,hotel);
             }
+        }
+
+        /// <summary>
+        /// UC 2 Gets the cheapest hotel.
+        /// </summary>
+        /// <param name="startDate">The start date.</param>
+        /// <param name="endDate">The end date.</param>
+        /// <returns></returns>
+        /// <exception cref="HotelReservationException">startDate is after endDate</exception>
+        public List<HotelDetails> GetCheapestHotel(DateTime startDate, DateTime endDate)
+        {
+            List<HotelDetails> hotelOutput = new List<HotelDetails>();
+            double costOfPresentHotel = 0;
+            double totalFare = 0;
+
+            // Check for proper start and end date
+            if (startDate > endDate)
+            {
+                throw new HotelReservationException(HotelReservationException.ExceptionType.INVALID_DATE_RANGE, "startDate is after endDate");
+            }
+
+            // Iterate through all the hotels
+            foreach (KeyValuePair<string, HotelDetails> hotel in regularHotelMap)
+            {
+                // Calculate total fare of each hotel
+                costOfPresentHotel = hotel.Value.GetTotalFare(startDate,endDate);
+
+                // If the cost of present hotel is less than previous 
+                if (costOfPresentHotel.CompareTo(totalFare) < 0 || totalFare == 0)
+                {
+                    hotelOutput.Clear();
+                    hotelOutput.Add(hotel.Value);
+                    totalFare = costOfPresentHotel;
+                }
+                
+                // If the cost of hotel is equal to least cost
+                else if(costOfPresentHotel.CompareTo(totalFare) == 0)
+                {
+                    hotelOutput.Add(hotel.Value);
+                }
+            }
+            return hotelOutput;
         }
     }
 }
