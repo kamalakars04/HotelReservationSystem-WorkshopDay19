e00c59bd24d1989fe555ea4cc9a8bd5746c67c30
kamalakars04 <kamalakars04@gmail.com>
2020-11-07 11:35:09 +0530

    [Kamalakar].[Refactor].Added UC1_AbilityToAddNewHotel

--- HotelReservationSystem.cs
+++ HotelReservationSystem.cs
@@ -4,11 +4,33 @@ using System.Text;
 
 namespace HotelReservationSystemWorkshop
 {
-    class HotelReservationSystem
+    class HotelReservationSystem : IAdmin
     {
+        static Dictionary<string, HotelDetails> regularHotelMap = new Dictionary<string, HotelDetails>();
+
         public HotelReservationSystem()
         {
             Console.WriteLine("Welcome to Hotel Reservation System");
+        }
+
+        static HotelReservationSystem()
+        {
+            IAdmin admin = new HotelReservationSystem();
+            admin.AddNewHotel(new HotelDetails("Lakewood", 110));
+            admin.AddNewHotel(new HotelDetails("Bridgewood", 150));
+            admin.AddNewHotel(new HotelDetails("Ridgewood", 220));
+        }
+
+        /// <summary>
+        /// UC 1 Adds the new hotel.
+        /// </summary>
+        /// <param name="hotel">The hotel.</param>
+        public void AddNewHotel(HotelDetails hotel)
+        {
+            if (!regularHotelMap.ContainsKey(hotel.hotelName))
+            {
+                regularHotelMap.Add(hotel.hotelName,hotel);
+            }
         }
     }
 }
