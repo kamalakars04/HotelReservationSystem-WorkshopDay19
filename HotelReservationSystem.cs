699140b7b10eb0e3ae8487cfac07b39b81260da5
kamalakars04 <kamalakars04@gmail.com>
2020-11-07 09:19:01 +0530

    Add project files.

---
+++ HotelReservationSystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystemWorkshop
+{
+    class HotelReservationSystem
+    {
+        public HotelReservationSystem()
+        {
+            Console.WriteLine("Welcome to Hotel Reservation System");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HotelReservationSystemWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
